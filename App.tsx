639aed95dc147d569a909a0adb7651501aee3c80
Hector Valenzuela <francisco1495@gmail.com>
2026-03-21 13:34:07 -0700

    set up this project

--- App.tsx
+++ App.tsx
@@ -3,11 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Login from 'app/screens/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <NavigationContainer>
+        <Stack.screen 
+          name='Login'
+          component={Login}
+          option={{ headerShown: false }}
+        />
+
+     
+      </NavigationContainer>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
---
+++ app/screens/Login.tsx
@@ -0,0 +1,23 @@
+import { View, TextInput, Text } from "react-native";
+
+const Login = () => {
+  return (
+    <View>
+      <Text>
+        PERN Stack Expense Tracker APP
+      </Text>
+      <View>
+        <TextInput
+          placeholder="Enter Text"
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder="Enter Text"
+        />
+      </View>
+    </View>
+  );
+}
+
+export default Login;
