0a92502c405daaf129d7cd1d236758e7b34bae63
Hector Valenzuela <francisco1495@gmail.com>
2026-03-21 18:30:32 -0700

    progress made during integrating backend

--- backend/db.ts
+++ backend/db.ts
@@ -1,3 +1,12 @@
-export const db = () => {
-  
-}
+import pkg from 'pg'
+const { Pool } = pkg
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: 'localhost',
+  database: process.env.DB,
+  port: parseInt(process.env.PORT),
+  password:  process.env.DB_PASSWORD,
+})
